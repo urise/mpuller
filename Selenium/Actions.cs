ad6e81a822e4cacc443b327d05ecdb473a61fed5
urise <urise@i.ua>
2015-08-13 22:17:39 +0300

    Initial

---
+++ Selenium/Actions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    public class Actions
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ Selenium/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actions = new Actions();
+            actions.Run();
+        }
+    }
+}
